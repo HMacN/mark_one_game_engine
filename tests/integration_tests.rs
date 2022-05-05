8c68d18d4fcbb608cb288bc794a98e905877d6d3
Hugh <hugh.macn@gmail.com>
2022-05-05 19:41:25 +0100

    Moved Bevy Related Tests Into Separate Directory
    As a continuation of spinning all the Bevy stuff into separate folders, I've moved the associated tests into a new folder too.

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,6 +1,9 @@
 extern crate materiel;
 
-mod isometric_game_grid_tests;
-mod in_game_location_tests;
 mod uid_tracker_tests;
-mod sprite_tracker_tests;
+mod tests_for_bevy_stuff
+{
+    mod in_game_location_tests;
+    mod isometric_game_grid_tests;
+    mod sprite_tracker_tests;
+}
---
+++ tests/tests_for_bevy_stuff/mod.rs
@@ -0,0 +1,3 @@
+pub mod in_game_location_tests;
+pub mod isometric_game_grid_tests;
+pub mod sprite_tracker_tests;
