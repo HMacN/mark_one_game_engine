f7d0580df178c4f35ea6c000cb3abf44cfad0c31
HMacN <hugh.macn@gmail.com>
2023-01-23 01:42:29 +0000

    Added WinItAdapter
    Changes:
    
    * MarkOneWindow now updates its record of the state of the WinIt window before making any changes to it.

--- src/view/mark_one_window.rs
+++ src/view/mark_one_window.rs
@@ -28,36 +28,43 @@ impl MarkOneWindow {
     }
 
     pub fn show_window(&mut self) {
+        self.retrieve_window_state();
         self.state.show();
         self.set_window_state();
     }
 
     pub fn hide_window(&mut self) {
+        self.retrieve_window_state();
         self.state.hide();
         self.set_window_state();
     }
 
     pub fn enter_fullscreen(&mut self) {
+        self.retrieve_window_state();
         self.state.go_fullscreen();
         self.set_window_state();
     }
 
     pub fn exit_fullscreen(&mut self) {
+        self.retrieve_window_state();
         self.state.exit_fullscreen();
         self.set_window_state();
     }
 
     pub fn minimise(&mut self) {
+        self.retrieve_window_state();
         self.state.minimise();
         self.set_window_state();
     }
 
     pub fn restore_from_minimised(&mut self) {
+        self.retrieve_window_state();
         self.state.restore();
         self.set_window_state();
     }
 
     pub fn set_window_title(&mut self, title: String) {
+        self.retrieve_window_state();
         self.state.set_title(title);
         self.set_window_state();
     }
