4c6c0f55521c5dffbf9f47ee4d5a13d5610dcfdb
HMacN <hugh.macn@gmail.com>
2023-10-10 21:45:52 +0100

    Dusting off project (again).
    * Minor tidying to the code.
    
    * Moving over to a new IDE (RustRover).
    
    * Starting to read Clean Architecture by R. C. Martin.  Expect major refactoring to follow.

--- tests/mark_one_game_engine_test_suite/win_it_adapter_tests.rs
+++ tests/mark_one_game_engine_test_suite/win_it_adapter_tests.rs
@@ -1,2 +1,8 @@
 // TODO add tests for WinIt adapter
 // Some of the functionality is definitely testable!
+
+// TODO Test initialise to default values.
+
+// TODO Test can change values.
+
+// TODO test can get things like the display area.
